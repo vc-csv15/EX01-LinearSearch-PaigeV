1bd0bdd777749cf1748f1e0eee13b3c5d87ea419
PaigeV <paige_vasquezhousle1@my.vcccd.edu>
2019-09-11 14:20:16 -0700

    Remember to use size_t as the variable type in the for loop and cancel nullptr

---
+++ src/linearsearch.cc
@@ -0,0 +1,15 @@
+//
+// Created by Paige Vasquez-Housley on 9/11/2019.
+//
+
+#include <cstdint>
+#include<cstdlib>
+
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+
+                template <class T>
+                int64_t find(const T& value, T array[], size_t size) {
+                    // put your code here
+                }
+
+            }}}}
